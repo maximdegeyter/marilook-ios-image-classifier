1d1ca5c5bce673ab842e605a5972fd3d339c37b8
maximdegeyter <maximdegeyter@outlook.com>
2019-12-03 00:56:45 +0100

    setup project

---
+++ project/project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project
+//
+//  Created by Maxim De Geyter on 03/12/2019.
+//  Copyright © 2019 Maxim De Geyter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
